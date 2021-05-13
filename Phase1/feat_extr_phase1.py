b6bd9b94fd9a78d7ed71b5b93a42f9158dd9d34f
SirHappi <sri13.vinukonda@gmail.com>
2021-05-13 17:43:51 -0400

    extracted eyes and nose

--- Phase1/feat_extr_phase1.py
+++ Phase1/feat_extr_phase1.py
@@ -8,22 +8,36 @@ import math
 
 def process_image(csv_name,imagename):
     print(csv_name)
-    x_min = 0 # X_17 round down
-    x_max = 0 # X_21 round up
-    y_min = 0 # X_19 round down
-    y_max = 0 # X_29 round up
+    eye_left_x = 0 # X_17 round down
+    eye_right_x = 0 # X_21 round up
+    eye_top_y = 0 # X_19 round down
+    eye_bot_y = 0 # X_29 round up
+
+    nose_left_x = 0 # X_31 round down
+    nose_right_x = 0 # X_21 round up
+    nose_top_y = 0 # X_19 round down
+    nose_bot_y = 0 # X_29 round up
+
+
+
     i = 0
     with open(csv_name,newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=' ', quotechar='|')
 
         for row in reader:
             
-            x_min = int((row["x_17,"]).split(".")[0])
-            x_max = math.ceil(float(row["x_21,"].strip(",")))
-            y_min = int(row["y_17,"].split(".")[0])
-            y_max = math.ceil(float(row["y_29,"].strip(",")))
-    assert type(imagename) == str
+            eye_left_x = int((row["x_17,"]).split(".")[0])
+            eye_right_x = math.ceil(float(row["x_27,"].strip(",")))
+            eye_top_y = int(row["y_21,"].split(".")[0])
+            eye_bot_y = math.ceil(float(row["y_29,"].strip(",")))
 
+            nose_left_x = int((row["x_39,"]).split(".")[0])
+            nose_right_x = math.ceil(float(row["x_35,"].strip(",")))
+            nose_top_y = int((row["y_38,"]).split(".")[0])
+            nose_bot_y = math.ceil(float(row["y_51,"].strip(",")))
+    assert type(imagename) == str
+    print(nose_top_y,nose_bot_y,nose_left_x,nose_right_x)
+    print(eye_top_y,eye_bot_y,eye_left_x,eye_right_x)
     #Read image and convert it to grayscale
     image = cv2.imread(imagename)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -31,11 +45,13 @@ def process_image(csv_name,imagename):
 
     
     #                Y-, Y+ : X-, X+
-    plt.imshow(image[y_min:y_max,x_min:x_max])
+    plt.imshow(image[eye_top_y:eye_bot_y,eye_left_x:eye_right_x])
+    plt.show()
+    plt.imshow(image[nose_top_y:nose_bot_y,nose_left_x:nose_right_x])
     plt.show()
 
     
 
 path_to_samples = "../../OpenFace/samples/"
 path_to_processed = "../../OpenFace/processed/"
-process_image(path_to_processed+"sample4.csv",path_to_samples+"sample4.jpg")
+process_image(path_to_processed+"sample1.csv",path_to_samples+"sample1.jpg")
