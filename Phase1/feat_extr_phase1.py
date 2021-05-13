cee31f5e98387e7eeb02b755c75835a95efb0b1f
SirHappi <sri13.vinukonda@gmail.com>
2021-05-13 19:28:59 -0400

    extracting mouth successfully

--- Phase1/feat_extr_phase1.py
+++ Phase1/feat_extr_phase1.py
@@ -18,6 +18,11 @@ def process_image(csv_name,imagename):
     nose_top_y = 0 # X_19 round down
     nose_bot_y = 0 # X_29 round up
 
+    mouth_left_x = 0 # X_48 round down
+    mouth_right_x = 0 # X_54 round up
+    mouth_top_y = 0 # X_51 round down
+    mouth_bot_y = 0 # X_57 round up
+
 
 
     i = 0
@@ -31,10 +36,16 @@ def process_image(csv_name,imagename):
             eye_top_y = int(row["y_21,"].split(".")[0])
             eye_bot_y = math.ceil(float(row["y_29,"].strip(",")))
 
-            nose_left_x = int((row["x_39,"]).split(".")[0])
-            nose_right_x = math.ceil(float(row["x_35,"].strip(",")))
+            nose_left_x = int((row["x_20,"]).split(".")[0])
+            nose_right_x = math.ceil(float(row["x_23,"].strip(",")))
             nose_top_y = int((row["y_38,"]).split(".")[0])
-            nose_bot_y = math.ceil(float(row["y_51,"].strip(",")))
+            nose_bot_y = math.ceil(float(row["y_33,"].strip(",")))
+            
+            mouth_left_x = int((row["x_48,"]).split(".")[0])
+            mouth_right_x = math.ceil(float(row["x_54,"].strip(",")))
+            mouth_top_y = (int((row["y_51,"]).split(".")[0])+int((row["y_33,"]).split(".")[0]))//2
+            mouth_bot_y = math.ceil(float(row["y_57,"].strip(","))*0.75+float(row["y_8,"].strip(","))*0.25)
+
     assert type(imagename) == str
     print(nose_top_y,nose_bot_y,nose_left_x,nose_right_x)
     print(eye_top_y,eye_bot_y,eye_left_x,eye_right_x)
@@ -42,16 +53,16 @@ def process_image(csv_name,imagename):
     image = cv2.imread(imagename)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-
-    
     #                Y-, Y+ : X-, X+
     plt.imshow(image[eye_top_y:eye_bot_y,eye_left_x:eye_right_x])
     plt.show()
     plt.imshow(image[nose_top_y:nose_bot_y,nose_left_x:nose_right_x])
+    plt.show()
+    plt.imshow(image[mouth_top_y:mouth_bot_y,mouth_left_x:mouth_right_x])
     plt.show()
 
     
 
 path_to_samples = "../../OpenFace/samples/"
 path_to_processed = "../../OpenFace/processed/"
-process_image(path_to_processed+"sample1.csv",path_to_samples+"sample1.jpg")
+process_image(path_to_processed+"MySample.csv",path_to_samples+"MySample.jpg")
